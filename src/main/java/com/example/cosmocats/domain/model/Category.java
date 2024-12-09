b56332edd83d76781c7f9552c2d1611b18379b77
Julya <julya.meleshko@gmail.com>
2024-12-09 22:37:31 +0200

    Delete files

--- src/main/java/com/example/cosmocats/domain/model/Category.java
+++
@@ -1,7 +0,0 @@
-package com.example.cosmocats.domain.model;
-
-public class Category {
-    private Long id;
-
-    private String name;
-}
--- src/main/java/com/example/cosmocats/dto/ProductDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.cosmocats.dto;
-
-public class ProductDTO {
-}
